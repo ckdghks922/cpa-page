e69f304a50c1b3cac5a92e95286ec45927d9c31c
changhwan_park <ch.park@kaist.ac.kr>
2022-03-29 23:07:53 +0900

    feat: 게시판 영역 view 1차 작업

--- src/components/Board/BoardView.tsx
+++ src/components/Board/BoardView.tsx
@@ -1,5 +1,77 @@
+import * as S from './styled';
+
+const TEMP_DATA = [
+  { id: 1, title: '게시글 1', author: '홍길동1', date: '2022/03/01' },
+  { id: 2, title: '게시글 2', author: '홍길동2', date: '2022/03/02' },
+  { id: 3, title: '게시글 3', author: '홍길동3', date: '2022/03/03' },
+  { id: 4, title: '게시글 4', author: '홍길동4', date: '2022/03/04' },
+  { id: 5, title: '게시글 5', author: '홍길동5', date: '2022/03/05' },
+];
+
+const BoardTitle: React.FC = () => {
+  return (
+    <thead>
+      <tr>
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>등록일</th>
+      </tr>
+    </thead>
+  );
+};
+
+interface ItemProps {
+  info: {
+    id: number;
+    title: string;
+    date: string;
+    author: string;
+  };
+}
+const BoardItem: React.FC<ItemProps> = ({ info }) => {
+  const { id, title, author, date } = info;
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{author}</td>
+      <td>{date}</td>
+    </tr>
+  );
+};
+
+const BottomPage: React.FC = () => {
+  return (
+    <S.PageList>
+      <S.PageItem>이전</S.PageItem>
+      <S.PageItem>1</S.PageItem>
+      <S.PageItem>2</S.PageItem>
+      <S.PageItem>3</S.PageItem>
+      <S.PageItem>4</S.PageItem>
+      <S.PageItem>5</S.PageItem>
+      <S.PageItem>다음</S.PageItem>
+    </S.PageList>
+  );
+};
+
 const BoardView: React.FC = () => {
-  return <div>This is Board</div>;
+  return (
+    <S.Wrapper>
+      <S.Title>문의 게시판</S.Title>
+      <S.SubTitle>문의 글을 작성할 수 있습니다.</S.SubTitle>
+      <table>
+        <BoardTitle />
+        <tbody>
+          {TEMP_DATA.map(elem => (
+            <BoardItem key={elem.id} info={elem} />
+          ))}
+        </tbody>
+      </table>
+      <S.WriteButton>글쓰기</S.WriteButton>
+      <BottomPage />
+    </S.Wrapper>
+  );
 };
 
 export default BoardView;
---
+++ src/components/Board/styled.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 200px;
+`;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h3``;
+
+export const WriteButton = styled.button`
+  width: 100px;
+  height: 30px;
+  margin-top: 20px;
+  margin-left: auto;
+  background-color: #444;
+  color: #fff;
+
+  &:hover {
+    background-color: #222;
+  }
+`;
+
+export const PageList = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+`;
+
+export const PageItem = styled.li`
+  margin: 0 10px;
+  cursor: pointer;
+`;
