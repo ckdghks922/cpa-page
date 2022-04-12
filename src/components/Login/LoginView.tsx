56e98c5dc092c1a87907523f47c7208681dbab28
changhwan.park <ch.park@kaist.ac.kr>
2022-04-12 19:36:34 +0900

    feat: 로그인 버튼 테스트

--- src/components/Login/LoginView.tsx
+++ src/components/Login/LoginView.tsx
@@ -1,7 +1,13 @@
 import * as S from './styled';
 
 const LoginView: React.FC = () => {
-  return <S.Wrapper />;
+  return (
+    <S.Wrapper>
+      원하는 소셜 계정을 통해 로그인 할 수 있습니다.
+      <S.LoginButton />
+      <S.LoginButton />
+    </S.Wrapper>
+  );
 };
 
 export default LoginView;
--- src/components/Login/styled.tsx
+++ src/components/Login/styled.tsx
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
+import Naver from '../../assets/naver.png';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LoginButton = styled.button`
+  width: 200px;
+  height: 50px;
+  margin-top: 30px;
+  cursor: pointer;
+  background: transparent url(${Naver}) no-repeat 0 0;
+  background-size: contain;
+  border: none;
+`;
