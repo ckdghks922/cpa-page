6b8eeb6b180d40a0c68324d3bf189c6787083072
changhwan_park <ch.park@kaist.ac.kr>
2022-04-13 23:23:53 +0900

    chore: 임시 주석 처리

--- src/components/Login/LoginView.tsx
+++ src/components/Login/LoginView.tsx
@@ -4,8 +4,8 @@ const LoginView: React.FC = () => {
   return (
     <S.Wrapper>
       <S.Text>원하는 소셜 계정을 활용하여 로그인 할 수 있습니다.</S.Text>
-      <S.NaverButton />
-      <S.KaKaoButton />
+      {/* <S.NaverButton />
+      <S.KaKaoButton /> */}
     </S.Wrapper>
   );
 };
